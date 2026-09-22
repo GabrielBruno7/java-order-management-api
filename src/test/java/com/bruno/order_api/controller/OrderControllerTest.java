b1bdb8c2857ee3ab4ab291b54c2cd4b1701070f2
GabrielBruno7 <gabriel.bruno1w@gmail.com>
2026-09-22 20:30:41 -0300

    test: add controller not found test

---
+++ src/test/java/com/bruno/order_api/controller/OrderControllerTest.java
@@ -0,0 +1,30 @@
+package com.bruno.order_api.controller;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.bruno.order_api.application.OrderService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturn404WhenOrderDoesNotExist() throws Exception {
+        UUID id = UUID.randomUUID();
+
+        mockMvc.perform(get("/orders/" + id))
+                .andExpect(status().isNotFound());
+    }
+}
